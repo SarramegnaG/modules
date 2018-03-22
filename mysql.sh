5927da5ceb988aaa42faf753377d1b32d13cd67a
Guillaume Sarramegna <guillaume.sarramegna@acelys.fr>
2018-03-22 11:19:06 +0100

    Add mycli install

--- mysql.sh
+++ mysql.sh
@@ -4,3 +4,8 @@
 debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 apt install mysql-server -y
+
+# Install mycli
+apt install python-pip -y
+pip install --upgrade pip
+pip install mycli
